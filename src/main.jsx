1dbfecfbbfe05495921037895f13112ba32920cc
dansut24 <Danieljamessutton18@outlook.com>
2026-09-06 15:08:32 +0100

    Route apex domain to the marketing application

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { MarketingApp } from './features/marketing/MarketingApp.jsx'
 import { resolveTenantSurface } from './lib/tenantSurface.js'
 
 const rootElement = document.getElementById('root')
@@ -10,8 +11,10 @@ document.documentElement.dataset.hi5Surface = initialSurface.kind
 document.body.dataset.hi5Surface = initialSurface.kind
 if (rootElement) rootElement.dataset.hi5Surface = initialSurface.kind
 
+const RootApp = initialSurface.kind === 'marketing' ? MarketingApp : App
+
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <RootApp />
   </StrictMode>,
 )
